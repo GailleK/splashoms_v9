af1c6392888aa9585fcffaede4b49d9592113468
GailleK <angelica.elman@gmail.com>
2026-02-14 10:53:53 +0800

    login authentication

--- oms_django/oms_django/urls.py
+++ oms_django/oms_django/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("oms_app.urls")),
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.jwt")),
 ]
-
